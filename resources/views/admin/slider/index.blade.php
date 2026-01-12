5f20943df0ca336959a18646f11037cc9d9a475b
kevin <kevinkalenga10@gmail.com>
2026-01-13 00:29:24 +0100

    Home Slider - Showing Data at Index Datatable

--- resources/views/admin/slider/index.blade.php
+++ resources/views/admin/slider/index.blade.php
@@ -1,24 +1,73 @@
- @extends('admin.layouts.master')  
-   
+@extends('admin.layouts.master')  
+
 @section('content')
 <section class="section">
-          <div class="section-header">
-            <h1>Slider</h1>
-          </div>
-        
-           <div class="card card-primary">
-                  <div class="card-header">
-                    <h4>Card Header</h4>
-                    <div class="card-header-action">
-                      <a href="#" class="btn btn-primary">
-                        Create New
-                      </a>
-                    </div>
-                  </div>
-                  <div class="card-body">
-                    <p>Write something here</p>
-                  </div>
-                </div>
-    
-        </section>
+    <div class="section-header">
+        <h1>Slider</h1>
+    </div>
+
+    <div class="card card-primary">
+        <div class="card-header">
+            <h4>Sliders List</h4>
+            <div class="card-header-action">
+                <a href="{{ route('sliders.create') }}" class="btn btn-primary">
+                    Create New
+                </a>
+            </div>
+        </div>
+
+        <div class="card-body">
+            <table class="table table-bordered" id="sliders-table">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Image</th>
+                        <th>Offer</th>
+                        <th>Title</th>
+                        <th>Sub Title</th>
+                        <th>Short Description</th>
+                        <th>Button Link</th>
+                        <th>Status</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+            </table>
+        </div>
+    </div>
+</section>
 @endsection
+
+@push('scripts')
+<script>
+$(function () {
+    $('#sliders-table').DataTable({
+        processing: true,
+        serverSide: true,
+        ajax: '{{ route("sliders.index") }}',
+        columns: [
+            { data: 'id', name: 'id' },
+            { data: 'image', name: 'image', orderable: false, searchable: false,
+              render: function(data, type, row) {
+                  return '<img src="'+data+'" width="80">';
+              }
+            },
+            { data: 'offer', name: 'offer' },
+            { data: 'title', name: 'title' },
+            { data: 'sub_title', name: 'sub_title' },
+            { data: 'short_description', name: 'short_description' },
+            { data: 'button_link', name: 'button_link',
+              render: function(data, type, row) {
+                  return '<a href="'+data+'" target="_blank">'+data+'</a>';
+              }
+            },
+            { data: 'status', name: 'status',
+              render: function(data) {
+                  return data ? 'Active' : 'Inactive';
+              }
+            },
+            { data: 'action', name: 'action', orderable: false, searchable: false }
+        ]
+    });
+});
+</script>
+@endpush
