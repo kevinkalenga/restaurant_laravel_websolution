20895a07cab46198bee73414ff04bc4b2976580b
kevin <kevinkalenga10@gmail.com>
2026-02-07 01:42:11 +0100

    Why choose us - Creating Necessary Files and Designs (Part- 2)

--- resources/views/admin/why-choose-us/index.blade.php
+++ resources/views/admin/why-choose-us/index.blade.php
@@ -6,14 +6,52 @@
         <h1>Why Choose Us</h1>
     </div>
 
+    <!-- <div class="card card-primary">
+        <div class="card-header">
+            <h4>All Items</h4>
+           
+        </div>
+
+    </div> -->
+   
+      <div class="card">
+        
+         <div class="card-body">
+           <div id="accordion">
+            <div class="accordion">
+               <div class="accordion-header collapsed bg-primary text-light p-3" role="button" data-toggle="collapse" data-target="#panel-body-1" aria-expanded="false">
+                   <h4>Why choose us section titles</h4>
+               </div>
+               <div class="accordion-body collapse" id="panel-body-1" data-parent="#accordion" style="">
+                  <div class="form-group">
+                     <label for="">Top Title</label>
+                     <input type="text" class="form-control" >
+                  </div>
+                  <div class="form-group">
+                     <label for="">Main Title</label>
+                     <input type="text" class="form-control" >
+                  </div>
+                  <div class="form-group">
+                     <label for="">Sub Title</label>
+                     <input type="text" class="form-control" >
+                  </div>
+                   <button class="btn btn-primary" type="submit">Save</button>
+               </div>
+            </div>
+           </div>
+         </div>
+      </div>
+   
+</section>
+<section class="section">
+    <div class="section-header">
+        <h1>Why Choose Us</h1>
+    </div>
+
     <div class="card card-primary">
         <div class="card-header">
             <h4>All Items</h4>
-            <div class="card-header-action">
-                <a href="{{ route('admin.sliders.create') }}" class="btn btn-primary">
-                    Create New
-                </a>
-            </div>
+           
         </div>
 
         <div class="card-body">
