d95bbfade1571742a8f0a1d56c560b2b21171ff5
kevin <kevinkalenga10@gmail.com>
2026-03-11 03:24:04 +0100

    Why Choose Us - Showing Dynamic Contents at Frontend

--- app/Http/Controllers/Frontend/FrontendController.php
+++ app/Http/Controllers/Frontend/FrontendController.php
@@ -5,13 +5,24 @@ namespace App\Http\Controllers\Frontend;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Slider;
+use App\Models\WhyChooseUs;
+use App\Models\SectionTitle;
 
 class FrontendController extends Controller
 {
     public function index()
     {
         $sliders = Slider::where('status', 1)->get();
-        return view('frontend.home.index', compact('sliders'));
+        $sectionTitles = $this->getSectionTitles();
+        $whyChooseUs = WhyChooseUs::where('status', 1)->get();
+        // dd($sectionTitles);
+        return view('frontend.home.index', compact('sliders', 'sectionTitles', 'whyChooseUs'));
        
+    }
+
+    public function getSectionTitles() 
+    {
+        $keys = ['why_choose_top_title', 'why_choose_main_title', 'why_choose_sub_title'];
+        return SectionTitle::whereIn('key', $keys)->pluck('value', 'key');
     }
 }
