39f7c1dd048d5196b9dbc7c8072fc05e03f1a4ca
kevin <kevinkalenga10@gmail.com>
2026-04-08 01:20:59 +0200

    Showing Products - Show Dynamic Product in Food Menu Section (Part - 2)

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -9,4 +9,10 @@ class Product extends Model
 {
     /** @use HasFactory<\Database\Factories\ProductFactory> */
     use HasFactory;
+    
+    // each(id or product) belong to a category => category_id column inside product
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
