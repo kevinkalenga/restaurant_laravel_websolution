b033dda0fe94f0540a92da2870a43a5e32b8d721
kevin <kevinkalenga10@gmail.com>
2026-04-24 00:04:22 +0200

    Add to Cart - Working with Add to Cart Feature (Part - 3)

--- app/Http/Controllers/Frontend/CartController.php
+++ app/Http/Controllers/Frontend/CartController.php
@@ -5,32 +5,55 @@ namespace App\Http\Controllers\Frontend;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Product;
+use Cart;
 
 class CartController extends Controller
 {
     public function addToCart(Request $request)
     {
-        //  dd($request->all());
-        // fetch the product from the database 
-        $product = Product::with(['productSizes', 'productOptions'])->findOrFail($request->product_id);
-        $productSize = $product->productSizes->where('id', $request->product_size)->first();
-        $productOptions = $product->productOptions->whereIn('id', $request->product_option)->first();
-        // where will be storing the product size
+        $product = Product::with(['productSizes', 'productOptions'])
+        ->findOrFail($request->product_id);
+
+        // SIZE (peut être null)
+        $productSize = $product->productSizes
+            ->firstWhere('id', $request->product_size);
+
+        // OPTIONS (collection)
+        $productOptions = $product->productOptions
+            ->whereIn('id', $request->product_option ?? []);
+
         $options = [
-            'product_size' => [
+            'product_size' => $productSize ? [
                 'id' => $productSize->id,
                 'name' => $productSize->name,
                 'price' => $productSize->price
-            ],
-            'product_options' => []
-        ]
+            ] : null,
 
-        foreach($productOptions as $option) {
+            'product_options' => [],
+
+            'product_info' => [
+                'image' => $product->thumb_image,
+                'slug' => $product->slug
+            ]
+        ];
+
+        foreach ($productOptions as $option) {
             $options['product_options'][] = [
                 'id' => $option->id,
                 'name' => $option->name,
                 'price' => $option->price
-            ]
+            ];
         }
+
+        Cart::add([
+            'id' => $product->id,
+            'name' => $product->name,
+            'qty' => $request->quantity ?? 1,
+            'price' => $product->offer_price > 0 ? $product->offer_price : $product->price,
+            'weight' => 0,
+            'options' => $options
+        ]);
+
+        return redirect()->back()->with('status', 'Product Added Into Cart Successfully!');
     }
 }
