5be694a9aaf66977b817b8c16b3f59322514a9f1
kevin <kevinkalenga10@gmail.com>
2026-06-21 01:39:19 +0200

    View Page Add to Cart - Working with Product View Add to Cart Calculation

--- resources/views/frontend/pages/product-view.blade.php
+++ resources/views/frontend/pages/product-view.blade.php
@@ -72,45 +72,52 @@
                             
                         </h3>
                         <p class="short_description">{!! $product->short_description !!}</p>
-                         @if ($product->productSizes()->exists())
-                          <div class="details_size">
-                          
-                          
-                            <h5>select size</h5>
-                            @foreach($product->productSizes as $productSize)
-                             <div class="form-check">
-                                 <input class="form-check-input" type="radio" name="flexRadioDefault" id="size-{{$productSize->id}}" checked>
-                                 <label class="form-check-label" for="size-{{$productSize->id}}">
-                                     {{$productSize->name}} <span>+ ${{$productSize->price}}</span>
-                                 </label>
-                              </div>
-                            @endforeach
-                          </div>
-                         @endif
-                          @if ($product->productOptions()->exists())
-                           <div class="details_extra_item">
-                             <h5>select option <span>(optional)</span></h5>
-                             @foreach($product->productOptions as $productOption)
-                              <div class="form-check">
-                                 <input class="form-check-input" type="checkbox" value="" id="option-{{$productOption->id}}">
-                                 <label class="form-check-label" for="option-{{$productOption->id}}">
-                                     {{$productOption->name}} <span>+ ${{$productOption->price}}</span>
-                                 </label>
-                               </div>
-                             @endforeach
-                           </div>
-                          @endif
-                        <div class="details_quentity">
-                            <h5>select quentity</h5>
-                            <div class="quentity_btn_area d-flex flex-wrapa align-items-center">
-                                <div class="quentity_btn">
-                                    <button class="btn btn-danger"><i class="fal fa-minus"></i></button>
-                                    <input type="text" placeholder="1">
-                                    <button class="btn btn-success"><i class="fal fa-plus"></i></button>
+                        <form action="">
+                            @csrf 
+
+                            <input type="hidden" name="base_price" class="v_base_price" value="{{$product->offer_price > 0 ? $product->offer_price : $product_price}}">
+                            @if ($product->productSizes()->exists())
+                                <div class="details_size">
+                                
+                                
+                                    <h5>select size</h5>
+                                    @foreach($product->productSizes as $productSize)
+                                    <div class="form-check">
+                                        <input class="form-check-input v_product_size" type="radio" name="flexRadioDefault"
+                                         id="size-{{$productSize->id}}" data-price="{{$productSize->price}}">
+                                        <label class="form-check-label" for="size-{{$productSize->id}}">
+                                            {{$productSize->name}} <span>+ ${{$productSize->price}}</span>
+                                        </label>
+                                    </div>
+                                    @endforeach
                                 </div>
-                                <h3>$320.00</h3>
+                            @endif
+                            @if ($product->productOptions()->exists())
+                                <div class="details_extra_item">
+                                    <h5>select option <span>(optional)</span></h5>
+                                    @foreach($product->productOptions as $productOption)
+                                    <div class="form-check">
+                                        <input class="form-check-input v_product_option" type="checkbox" value=""
+                                         id="option-{{$productOption->id}}" data-price="{{$productOption->price}}">
+                                        <label class="form-check-label" for="option-{{$productOption->id}}">
+                                            {{$productOption->name}} <span>+ ${{$productOption->price}}</span>
+                                        </label>
+                                    </div>
+                                    @endforeach
+                                </div>
+                            @endif
+                            <div class="details_quentity">
+                                <h5>select quentity</h5>
+                                <div class="quentity_btn_area d-flex flex-wrapa align-items-center">
+                                    <div class="quentity_btn">
+                                        <button class="btn btn-danger"><i class="fal fa-minus"></i></button>
+                                        <input type="text" placeholder="1">
+                                        <button class="btn btn-success"><i class="fal fa-plus"></i></button>
+                                    </div>
+                                    <h3 id="v_total_price">$320.00</h3>
+                                </div>
                             </div>
-                        </div>
+                        </form>
                         <ul class="details_button_area d-flex flex-wrap">
                             <li><a class="common_btn" href="#">add to cart</a></li>
                             <li><a class="wishlist" href="#"><i class="far fa-heart"></i></a></li>
@@ -390,3 +397,45 @@
 
 
 @endsection
+
+@push('scripts')
+
+  <script>
+     $(document).ready(function(){
+        $('.v_product_size').on('change', function(){
+            v_updateTotalPrice()
+          
+        })
+        $('.v_product_option').on('change', function(){
+           v_updateTotalPrice()
+        })
+
+          // Function to update the total price base on selected option
+        function v_updateTotalPrice() {
+          let basePrice = parseFloat($('.v_base_price').val())
+          let selectedSizePrice = 0;
+          let selectedOptionsPrice = 0; 
+          let quantity = parseFloat($('#quantity').val())
+
+          // Calculate the selected size price 
+          let selectedSize = $('.v_product_size:checked') ;
+          
+          if(selectedSize.length > 0) {
+            selectedSizePrice = parseFloat(selectedSize.data("price"))
+           
+          }
+          // Calculate selected options price 
+          let selectedOptions = $('.v_product_option:checked')
+          $(selectedOptions).each(function(){
+            selectedOptionsPrice += parseFloat($(this).data("price"))
+          })
+
+          // Calculate the total price 
+          let totalPrice = (basePrice + selectedSizePrice + selectedOptionsPrice);
+
+          $('#v_total_price').text("{{config('settings.site_currency_icon')}}" + totalPrice);
+        }
+     })
+  </script>
+
+@endpush
