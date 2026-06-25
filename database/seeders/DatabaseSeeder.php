289873e685b423aee3aaaae56996f92a099233ae
kevin <kevinkalenga10@gmail.com>
2026-06-26 01:47:47 +0200

    2_Coupon Feature-Creating Migration Columns And Seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Slider;
 use App\Models\Product;
+use App\Models\Coupon;
 
 class DatabaseSeeder extends Seeder
 {
@@ -32,6 +33,7 @@ class DatabaseSeeder extends Seeder
         $this->call(CategorySeeder::class);
         // php artisan tinker et  \App\Models\Product::factory(5)->create()
         Product::factory(10)->create();
+        Coupon::factory(3)->create();
         
     }
 }
