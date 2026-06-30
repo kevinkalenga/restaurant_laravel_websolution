36183c88db36518c0ff991a19fe8f76975d5b59d
kevin <kevinkalenga10@gmail.com>
2026-07-01 01:23:51 +0200

    2_Customer Address - Creating Migration Columns

--- database/migrations/2026_06_30_224126_create_addresses_table.php
+++ database/migrations/2026_06_30_224126_create_addresses_table.php
@@ -8,11 +8,20 @@ return new class extends Migration
 {
     /**
      * Run the migrations.
+     * users the name of the table
      */
     public function up(): void
     {
         Schema::create('addresses', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('delivery_area_id')->constrained('delivery_areas');
+            $table->string('first_name');
+            $table->string('last_name')->nullable();
+            $table->string('email');
+            $table->string('phone');
+            $table->text('address');
+            $table->string('type');
             $table->timestamps();
         });
     }
