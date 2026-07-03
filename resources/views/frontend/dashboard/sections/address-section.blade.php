68adc0d3b0422f0a555bb8b5fdb77fc701fcf9ec
kevin <kevinkalenga10@gmail.com>
2026-07-03 14:03:57 +0200

    Customer Address - Fixing the Select Input Design Issue

--- resources/views/frontend/dashboard/sections/address-section.blade.php
+++ resources/views/frontend/dashboard/sections/address-section.blade.php
@@ -124,7 +124,7 @@
                                                             
                                                             <div class="col-md-12 col-lg-12 col-x-12">
                                                                 <div class="fp__check_single_form">
-                                                                    <select id="select_js4" name="delivery_area_id">
+                                                                    <select class="nice-select" name="delivery_area_id">
                                                                         <option value="">Select Area</option>
                                                                         @foreach($deliveryAreas as $area)
                                                                         <option @selected($address->delivery_area_id === $area->id) value="{{$area->id}}">{{$area->area_name}}</option>
