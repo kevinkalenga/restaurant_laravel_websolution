4cb0095c04df11f9881d0554a69aeaf8fc6ccdf6
kevin <kevinkalenga10@gmail.com>
2026-07-04 01:41:41 +0200

    Checkout - Working with Checkout Page(Part-2)

--- app/Http/Controllers/Frontend/CheckoutController.php
+++ app/Http/Controllers/Frontend/CheckoutController.php
@@ -4,11 +4,16 @@ namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Address;
+use App\Models\DeliveryArea;
+use Auth;
 
 class CheckoutController extends Controller
 {
     public function index()
     {
-        return view('frontend.pages.checkout');
+        $addresses = Address::where('user_id', Auth()->user()->id)->get();
+         $deliveryAreas = DeliveryArea::where('status', 1)->get();
+        return view('frontend.pages.checkout', compact('addresses', 'deliveryAreas'));
     }
 }
