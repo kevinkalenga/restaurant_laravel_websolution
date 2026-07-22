d02c062561922ce6168182105baf643766046833
kevin <kevinkalenga10@gmail.com>
2026-07-22 17:12:14 +0200

    6_Paypal Integration - Opening a Paypal Sandbox Account

--- app/Http/Controllers/Admin/PaymentGatewaySettingController.php
+++ app/Http/Controllers/Admin/PaymentGatewaySettingController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\PaymentGatewaySetting;
 use App\Traits\FileUploadTrait;
+use App\Services\PaymentGatewaySettingService;
 
 class PaymentGatewaySettingController extends Controller
 {
