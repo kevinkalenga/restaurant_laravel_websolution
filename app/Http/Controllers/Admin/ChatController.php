169056f97fec153e4fd4798aa4e8beb2189cb131
kevin <kevinkalenga10@gmail.com>
2026-09-10 00:11:55 +0200

    Real Time Chatting - Show Chat User List at Admin(Part-2)

--- app/Http/Controllers/Admin/ChatController.php
+++ app/Http/Controllers/Admin/ChatController.php
@@ -4,11 +4,22 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class ChatController extends Controller
 {
     public function index()
     {
+      //dd(auth()->user()->chats);
+      $userId = auth()->user()->id;
+      // chats is the relation
+      $chatUsers = User::where('id', '!=', $userId)->whereHas('chats', function($query) use ($userId){
+        $query->where(function($subQuery) use ($userId){
+          $subQuery->where('sender_id', $userId)->orWhere('receiver_id', $userId);
+        });
+      })->orderByDesc('created_at')->distinct()->get();
+
+      dd($chatUsers);
       return view('admin.chat.index');
     }
 }
