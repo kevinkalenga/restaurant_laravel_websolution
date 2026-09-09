6eac3b6cacc252eca1f52e2d02fa24c2c67417e6
kevin <kevinkalenga10@gmail.com>
2026-09-10 00:46:49 +0200

    Real Time Chatting - Show Chat User List at Admin(Part-3)

--- app/Http/Controllers/Admin/ChatController.php
+++ app/Http/Controllers/Admin/ChatController.php
@@ -8,6 +8,17 @@ use App\Models\User;
 
 class ChatController extends Controller
 {
+    /**
+     * Récupère la liste des utilisateurs ayant une conversation
+     * avec l'utilisateur actuellement connecté.
+     *
+     * Exclut l'utilisateur connecté de la liste et recherche uniquement
+     * les utilisateurs associés à une conversation où il est soit
+     * l'expéditeur, soit le destinataire.
+     *
+     * Les utilisateurs sont triés du plus récent au plus ancien
+     * et les doublons sont supprimés.
+     */
     public function index()
     {
       //dd(auth()->user()->chats);
@@ -19,7 +30,7 @@ class ChatController extends Controller
         });
       })->orderByDesc('created_at')->distinct()->get();
 
-      dd($chatUsers);
-      return view('admin.chat.index');
+      //dd($chatUsers);
+      return view('admin.chat.index', compact('chatUsers'));
     }
 }
