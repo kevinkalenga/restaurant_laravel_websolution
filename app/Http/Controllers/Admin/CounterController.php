a756042ac93537f464502fc86a51939337ced383
kevin <kevinkalenga10@gmail.com>
2026-09-23 02:18:50 +0200

    Counter Section - Working with Create Feature(Part-2)

--- app/Http/Controllers/Admin/CounterController.php
+++ app/Http/Controllers/Admin/CounterController.php
@@ -4,15 +4,101 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Traits\FileUploadTrait;
+use App\Models\Counter;
 
 class CounterController extends Controller
 {
+    use FileUploadTrait;
+    
     public function index()
     {
-        return view('admin.counter.index');
+        $counter = Counter::first();
+
+      return view('admin.counter.index', compact('counter'));
     }
+    
+    
     public function update(Request $request)
     {
-      dd($request->all());
+        $counter = Counter::first();
+
+        $request->validate([
+            'background' => 'nullable|image|max:2048',
+
+            'counter_icon_one' => 'required|string|max:255',
+            'counter_count_one' => 'required|string|max:255',
+            'counter_name_one' => 'required|string|max:255',
+
+            'counter_icon_two' => 'required|string|max:255',
+            'counter_count_two' => 'required|string|max:255',
+            'counter_name_two' => 'required|string|max:255',
+
+            'counter_icon_three' => 'required|string|max:255',
+            'counter_count_three' => 'required|string|max:255',
+            'counter_name_three' => 'required|string|max:255',
+
+            'counter_icon_four' => 'required|string|max:255',
+            'counter_count_four' => 'required|string|max:255',
+            'counter_name_four' => 'required|string|max:255',
+        ]);
+
+        $data = [
+            'counter_icon_one' => $request->counter_icon_one,
+            'counter_count_one' => $request->counter_count_one,
+            'counter_name_one' => $request->counter_name_one,
+
+            'counter_icon_two' => $request->counter_icon_two,
+            'counter_count_two' => $request->counter_count_two,
+            'counter_name_two' => $request->counter_name_two,
+
+            'counter_icon_three' => $request->counter_icon_three,
+            'counter_count_three' => $request->counter_count_three,
+            'counter_name_three' => $request->counter_name_three,
+
+            'counter_icon_four' => $request->counter_icon_four,
+            'counter_count_four' => $request->counter_count_four,
+            'counter_name_four' => $request->counter_name_four,
+        ];
+
+        /*
+        |--------------------------------------------------------------------------
+        | Background
+        |--------------------------------------------------------------------------
+        */
+        if ($request->hasFile('background')) {
+
+            if (
+                $counter &&
+                $counter->background &&
+                file_exists(public_path($counter->background))
+            ) {
+                unlink(public_path($counter->background));
+            }
+
+            $data['background'] = $this->uploadImage(
+                $request,
+                'background',
+                'uploads'
+            );
+        }
+
+        /*
+        |--------------------------------------------------------------------------
+        | Create / Update
+        |--------------------------------------------------------------------------
+        */
+        if ($counter) {
+
+            $counter->update($data);
+
+        } else {
+
+            Counter::create($data);
+        }
+
+        return redirect()
+            ->route('admin.counter.index')
+            ->with('success', 'Counter updated successfully!');
     }
 }
--- resources/views/admin/counter/index.blade.php
+++ resources/views/admin/counter/index.blade.php
@@ -15,97 +15,92 @@
         <div class="card-body">
             <form action="{{ route('admin.counter.update') }}" method="POST" enctype="multipart/form-data" novalidate>
                 @csrf
-                 @method('PUT')
+                @method('PUT')
+
                 <div class="form-group">
                    <label for="image-upload">Background</label>
                     <div id="image-preview" class="image-preview">
                       <label for="image-upload" id="image-label">
                           Choose File
                       </label>
-                      <input type="file" name="background" id="image-upload" class="form-control" >
+                      <input type="file" name="background" id="image-upload" class="form-control">
                     </div>
                    
                 </div>
                 
                 <h6>Counter One</h6>
                 <hr>
+
                 <div class="form-group">
                     <label>Counter Icon One</label>
-                     <button class="btn btn-secondary" role="iconpicker" name="counter_icon_one"></button>
+                    <button type="button" class="btn btn-secondary" role="iconpicker" name="counter_icon_one" data-icon="{{$counter->counter_icon_one}}"></button>
                 </div>
                    
                 <div class="form-group">
                     <label>Counter Count One</label>
-                    <input type="text" name="counter_count_one" class="form-control">
-                   
+                    <input type="text" name="counter_count_one" class="form-control" value="{{$counter->counter_count_one}}">
                 </div>
 
                 <div class="form-group">
                     <label>Counter Count Name</label>
-                    <input type="text" name="counter_name_one" class="form-control">
-                   
+                    <input type="text" name="counter_name_one" class="form-control" value="{{$counter->counter_name_one}}">
                 </div>
                 
                 <h6>Counter Two</h6>
                 <hr>
+
                 <div class="form-group">
                     <label>Counter Icon Two</label>
-                     <button class="btn btn-secondary" role="iconpicker" name="counter_icon_two"></button>
+                    <button class="btn btn-secondary" role="iconpicker" name="counter_icon_two" data-icon="{{$counter->counter_icon_two}}"></button>
                 </div>
                    
                 <div class="form-group">
                     <label>Counter Count Two</label>
-                    <input type="text" name="counter_count_two" class="form-control">
-                   
+                    <input type="text" name="counter_count_two" class="form-control" value="{{$counter->counter_count_two}}">
                 </div>
 
                 <div class="form-group">
                     <label>Counter Count Name Two</label>
-                    <input type="text" name="counter_name_two" class="form-control">
-                   
+                    <input type="text" name="counter_name_two" class="form-control" value="{{$counter->counter_name_two}}">
                 </div>
                 
                 <h6>Counter Three</h6>
                 <hr>
+
                 <div class="form-group">
                     <label>Counter Icon Three</label>
-                     <button class="btn btn-secondary" role="iconpicker" name="counter_icon_three"></button>
+                    <button class="btn btn-secondary" role="iconpicker" name="counter_icon_three" data-icon="{{$counter->counter_icon_three}}"></button>
                 </div>
                    
                 <div class="form-group">
                     <label>Counter Count Three</label>
-                    <input type="text" name="counter_count_three" class="form-control">
-                   
+                    <input type="text" name="counter_count_three" class="form-control" value="{{$counter->counter_count_three}}">
                 </div>
 
                 <div class="form-group">
                     <label>Counter Count Name Three</label>
-                    <input type="text" name="counter_name_three" class="form-control">
-                   
+                    <input type="text" name="counter_name_three" class="form-control" value="{{$counter->counter_name_three}}">
                 </div>
                 
                 <h6>Counter Four</h6>
                 <hr>
+
                 <div class="form-group">
                     <label>Counter Icon Four</label>
-                     <button class="btn btn-secondary" role="iconpicker" name="counter_icon_four"></button>
+                    <button class="btn btn-secondary" role="iconpicker" name="counter_icon_four" data-icon="{{$counter->counter_icon_four}}"></button>
                 </div>
                    
                 <div class="form-group">
                     <label>Counter Count Four</label>
-                    <input type="text" name="counter_count_four" class="form-control">
-                   
+                    <input type="text" name="counter_count_four" class="form-control" value="{{$counter->counter_count_four}}">
                 </div>
 
                 <div class="form-group">
                     <label>Counter Count Name Four</label>
-                    <input type="text" name="counter_name_four" class="form-control">
-                   
+                    <input type="text" name="counter_name_four" class="form-control" value="{{$counter->counter_name_four}}">
                 </div>
-                
 
-               
-                <button type="submit" class="btn btn-primary">Create</button>
+                <button type="submit" class="btn btn-primary">Update</button>
                 
             </form>
         </div>
@@ -113,3 +108,16 @@
 </section>
 @endsection
 
+
+@push('scripts') 
+  <script>
+     $(document).ready(function(){
+        $('.image-preview').css({
+          'background-image': 'url({{asset($counter->background)}})',
+          'background-size': 'cover',
+          'background-position': 'center center'
+        })
+     })
+  </script>
+
+@endpush
