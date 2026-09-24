f1cb4910caf5713b7730694e33381c3f3c4f6b19
kevin <kevinkalenga10@gmail.com>
2026-09-24 09:03:25 +0200

    Blog - Creating Necessary Files and Designs

---
+++ resources/views/admin/blog/index.blade.php
@@ -0,0 +1,50 @@
+@extends('admin.layouts.master')
+
+@section('content')
+
+<section class="section">
+
+```
+<div class="section-header">
+    <h1>Blog</h1>
+</div>
+
+<div class="card card-primary">
+
+    <div class="card-header">
+        <h4>All Blogs</h4>
+
+        <div class="card-header-action">
+            <a href="{{ route('admin.blog-category.create') }}"
+               class="btn btn-primary">
+                Create New
+            </a>
+        </div>
+    </div>
+
+    <div class="card-body">
+
+        <table class="table table-bordered" id="blog-categories-table">
+
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Slug</th>
+                    <th>Status</th>
+                    <th>Created At</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+
+        </table>
+
+    </div>
+
+</div>
+```
+
+</section>
+
+@endsection
+
--- routes/admin.php
+++ routes/admin.php
@@ -23,6 +23,7 @@ use App\Http\Controllers\Admin\ChefController;
 use App\Http\Controllers\Admin\TestimonialController;
 use App\Http\Controllers\Admin\CounterController;
 use App\Http\Controllers\Admin\BlogCategoryController;
+use App\Http\Controllers\Admin\BlogController;
 use App\Http\Controllers\Admin\AppDownloadSectionController;
 
 
@@ -131,8 +132,12 @@ Route::resource('testimonial', TestimonialController::class);
 Route::get('counter', [CounterController::class, 'index'])->name('counter.index');
 Route::put('counter', [CounterController::class, 'update'])->name('counter.update');
 
-//Blogs Routes 
+//Blogs category Routes 
 Route::resource('blog-category', BlogCategoryController::class);
+
+
+//Blogs Routes 
+Route::resource('blogs', BlogController::class);
 
 
 
